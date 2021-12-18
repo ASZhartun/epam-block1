067355b2988080474fda9e171e1d205bb870267e
Anatoliy Zhartun <as.zhartun@yandex.by>
2021-12-18 19:06:41 +0300

    Cycles block init. Task01: Solution is done.

---
+++ src/taskBlock/cycles/task01.java
@@ -0,0 +1,20 @@
+package taskBlock.cycles;
+
+import taskBlock.utilities.GetInput;
+
+/**
+ * 1. Напишите программу, где пользователь вводит <b>любое целое положительное число</b>.
+ * А программа суммирует все числа от 1 до введенного пользователем числа.
+ */
+public class task01 {
+    public static void main(String[] args) {
+        GetInput getInput = new GetInput();
+        double number = Math.abs(Double.parseDouble(getInput.scan()));
+        getInput.close();
+        int result = 0;
+        for (int i = 1; i < number; i++) {
+            result+=i;
+        }
+        System.out.println(result);
+    }
+}
