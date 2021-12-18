73b3df32b468b159959e0e58dfbe7a45161e7360
Anatoliy Zhartun <as.zhartun@yandex.by>
2021-12-18 12:39:12 +0300

    Solution of Task1 of Linear programming.

---
+++ src/taskBlock/linearProgram/task1.java
@@ -0,0 +1,19 @@
+package taskBlock.linearProgram;
+
+import taskBlock.utilities.GetInput;
+
+/**
+ * 1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
+ */
+
+public class task1 {
+    public static void main(String[] args) {
+        GetInput getInput = new GetInput();
+        int a = Integer.parseInt(getInput.scan());
+        int b = Integer.parseInt(getInput.scan());
+        int c = Integer.parseInt(getInput.scan());
+        getInput.close();
+        int z = ((a - 3) * b / 2) + c;
+        System.out.println("Result: " + z);
+    }
+}
